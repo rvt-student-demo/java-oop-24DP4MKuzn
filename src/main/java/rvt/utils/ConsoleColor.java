c9027a3336f2fabba21423c12ef0ae143dfe14de
Matvejs Kuznecovs <A242564MK@rvt.lv>
2026-03-03 10:45:10 +0200

    Improvements to the StudentRegistrator: Fixed LocalTime formatting and added coloration to data table

---
+++ src/main/java/rvt/utils/ConsoleColor.java
@@ -0,0 +1,23 @@
+package rvt.utils;
+
+public enum ConsoleColor {
+    BLACK("\u001B[30m"), 
+    RED("\u001B[31m"), 
+    GREEN("\u001B[32m"), 
+    YELLOW("\u001B[33m"), 
+    BLUE("\u001B[34m"),
+    PURPLE("\u001B[35m"), 
+    CYAN("\u001B[36m"), 
+    WHITE("\u001B[37m"),
+    RESET("\u001B[0m");
+
+    final String code;
+
+    ConsoleColor(String code) {
+        this.code = code;
+    }
+
+    public String getCode() {
+        return this.code;
+    }
+}
